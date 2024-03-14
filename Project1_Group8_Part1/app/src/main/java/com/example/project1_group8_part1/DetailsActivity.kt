8a8fd2b1d17ce9e577a6530a9f5f9365f4386dce
AzmiSadath <163077111+AzmiSadath@users.noreply.github.com>
2024-03-14 12:06:48 -0400

    Renamed the xml files and created 2 new models

--- Project1_Group8_Part1/app/src/main/java/com/example/project1_group8_part1/DetailsActivity.kt
+++ Project1_Group8_Part1/app/src/main/java/com/example/project1_group8_part1/DetailsActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.details)
+        setContentView(R.layout.activity_details)
     }
 
 }
